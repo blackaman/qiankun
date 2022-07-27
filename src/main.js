cf5c157151f7fea550eb91aae585573e3174148e
blackman <1021222766@qq.com>
2022-07-27 11:09:48 +0800

    build有问题

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { registerMicroApps, start } from 'qiankun';
-registerMicroApps([
-  {
-    name: 'micro-app-vue', // app name registered
-    entry: '//localhost:8082/child/vue-history/',
-    container: '#container',
-    activeRule: '/hexo',
-  },
-]);
+import {createApp} from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import { registerMicroApps, start} from 'qiankun';
+
+registerMicroApps([{
+  name: 'micro-app-vue', // app name registered
+  entry: '//localhost:8081/c-vue/',
+  container: '#container',
+  activeRule: '/vue',
+}, ]);
 
 start();
-createApp(App).mount('#qiankun')
+// Vue.config.productionTip = false;
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
 
-
+createApp(App).use(router).mount('#qiankun');
